7edc67856d0fdeb23c9bf48dccc000de05a2aa0f
jay <jaypifiverr@gmail.com>
2022-07-07 17:10:54 -0400

    commit tree 159a1695b7de0d87364a4454b12a17719cfb5a9f

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    return renderBooks(response.json)})
+
+  // .then((response) => response.json())
+  // .then((json) => renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
-  
+  //fetch request GOT api
+  //returned response should be converted to .json
+  //calls second function renderBooks(JSON-data)
 }
 
 function renderBooks(books) {
